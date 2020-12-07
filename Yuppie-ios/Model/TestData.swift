c046c62c014b4ce98f561d2cd6394f20108d3fbc
ryancao1997 <56422732+ryancao1997@users.noreply.github.com>
2020-12-07 15:12:45 -0800

    Update TestData.swift

--- Yuppie-ios/Model/TestData.swift
+++ Yuppie-ios/Model/TestData.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 
+import UIKit
+
+
 enum TestData {
     
     static let cards = [
@@ -29,5 +32,61 @@ enum TestData {
          serviceLogo: UIImage(named: "bozuttologo")!,
          color: UIImage(named: "bozutto1")!)
     ]
-    
+    static let buildings = [
+        Building(
+            id: "395",
+            name: "The Atellier",
+            images: ["bozutto1", "bozutto2"],
+            description: "Live life in luxary in one of the best apartment buildings in the city. 60 story Hudson Yards skyscraper just minutes away fro the mid-town.",
+            address: "635 W 42nd Street, New York, NY, 10036",
+            amenities: ["Pool", "Gym"],
+            units: [
+                Unit(
+                    id: "1",
+                    name: "40H",
+                    bedrooms: "2",
+                    bathrooms: "2",
+                    price: "3000",
+                    sqft: "2000"
+                )
+            ]
+            ),
+        Building(
+            id: "396",
+            name: "The Atellie",
+            images: ["bozutto1", "bozutto2"],
+            description: "Live life in luxary in one of the best apartment buildings in the city. 60 story Hudson Yards skyscraper just minutes away fro the mid-town.",
+            address: "635 W 42nd Street, New York, NY, 10036",
+            amenities: ["Pool", "Gym"],
+            units: [
+                Unit(
+                    id: "1",
+                    name: "40H",
+                    bedrooms: "2",
+                    bathrooms: "2",
+                    price: "3000",
+                    sqft: "2000"
+                )
+            ]
+            ),
+        Building(
+            id: "397",
+            name: "The Atelli",
+            images: ["bozutto1", "bozutto2"],
+            description: "Live life in luxary in one of the best apartment buildings in the city. 60 story Hudson Yards skyscraper just minutes away fro the mid-town.",
+            address: "635 W 42nd Street, New York, NY, 10036",
+            amenities: ["Pool", "Gym"],
+            units: [
+                Unit(
+                    id: "1",
+                    name: "40H",
+                    bedrooms: "2",
+                    bathrooms: "2",
+                    price: "3000",
+                    sqft: "2000"
+                )
+            ]
+            ),
+        
+    ]
 }
